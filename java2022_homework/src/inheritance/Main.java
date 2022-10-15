62f3ce22b8a9d768c65b9e6b665669075fffe601
sinanozkan5 <113855180+sinanozkan5@users.noreply.github.com>
2022-10-15 16:52:14 +0300

    Add files via upload

---
+++ java2022_homework/src/inheritance/Main.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+		
+		EmployeeManager empyoleeManager = new EmployeeManager();
+		CustomerManager customerManager = new CustomerManager();
+		
+	}
+}
